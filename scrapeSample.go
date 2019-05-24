264d2e387d3818380dff85c4d4aa437c8fd625cc
katsuya <katu7414@gmail.com>
2019-05-24 16:11:12 +0900

    Create scrapeSample.go

---
+++ scrapeSample.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+    // 投稿した記事の一覧ページのリンクから、ドキュメントを取得
+    doc, err := goquery.NewDocument("https://www.fox-hound.tech/author/kazuki-matsumoto/")
+    if err != nil {
+        fmt.Println(err)
+    }
+    // 投稿した記事のリンクのurlを配列に格納
+    var urls []string
+    doc.Find(".title a").Each(func(_ int, s *goquery.Selection) {
+        url, _ := s.Attr("href")
+        urls = append(urls, url)
+    })
+    // 出力
+    fmt.Println(urls)
+}
