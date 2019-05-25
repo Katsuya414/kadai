795dcd7ede91f559aa3ba23d5cbfb0d6e858527f
katsuya <katu7414@gmail.com>
2019-05-26 06:48:38 +0900

    Update sc.py

--- sc.py
+++ sc.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('http://test.neet-ai.com')
+response = requests.get('https://folio-sec.com/theme/virtual-reality')
 soup = BeautifulSoup(response.text,'lxml')
 title = soup.title.string
 print(title)
